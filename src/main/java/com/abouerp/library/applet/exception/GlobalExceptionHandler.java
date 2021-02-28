497f29d3e21bd2be2021e4f18d41cba14f58427e
Abouerp <1057240821@qq.com>
2021-03-01 01:28:05 +0800

    add exception

--- src/main/java/com/abouerp/library/applet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/abouerp/library/applet/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.abouerp.library.applet.exception;
 import com.abouerp.library.applet.bean.ResultBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,14 +13,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static ResponseEntity<ResultBean<Object>> handleException(ClientErrorException exception) {
+        return ResponseEntity.status(exception.getCode())
+                .body(exception.getResultBean());
+    }
+
+    private static ResponseEntity<ResultBean<String>> handleException(ResultBean<String> resultBean) {
+        return ResponseEntity.status(resultBean.getCode())
+                .body(resultBean);
+    }
+
     @ExceptionHandler(ClientErrorException.class)
     public HttpEntity<ResultBean<Object>> clientErrorException(ClientErrorException ex) {
         return handleException(ex);
     }
 
-    private static ResponseEntity<ResultBean<Object>> handleException(ClientErrorException exception) {
-        return ResponseEntity.status(exception.getCode())
-                .body(exception.getResultBean());
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Object httpRequestMethodNotSupportedException() {
+        return handleException(ResultBean.of(405, "Method Not Allowed"));
     }
 
 }
