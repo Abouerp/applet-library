aae59194e96b948af3a178cdfeb8ddd26af659bd
Abouerp <1057240821@qq.com>
2021-03-01 01:28:41 +0800

    review loadUsername

--- src/main/java/com/abouerp/library/applet/security/UserDetailsServiceImpl.java
+++ src/main/java/com/abouerp/library/applet/security/UserDetailsServiceImpl.java
@@ -4,17 +4,19 @@ import com.abouerp.library.applet.domain.Administrator;
 import com.abouerp.library.applet.exception.UnauthorizedException;
 import com.abouerp.library.applet.mapper.AdministratorMapper;
 import com.abouerp.library.applet.repository.AdministratorRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.regex.Pattern;
 
 /**
  * @author Abouerp
  */
+@Slf4j
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -35,10 +37,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String s) {
         Administrator administrator = administratorRepository.findByMobile(s).orElse(null);
         if (administrator == null) {
-            throw new UnauthorizedException();
-        }
-        String password = httpServletRequest.getParameter("password");
-        if (Boolean.FALSE.equals(passwordEncoder.matches(password, administrator.getPassword()))) {
             throw new UnauthorizedException();
         }
         return AdministratorMapper.INSTANCE.toUserPrincipal(administrator);
