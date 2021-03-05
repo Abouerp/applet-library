93d0dee72ac89c6c419cfb21471b543814243187
Abouerp <1057240821@qq.com>
2021-03-05 11:47:53 +0800

    add findByBookId for getAll message

--- src/main/java/com/abouerp/library/applet/controller/BookDetailController.java
+++ src/main/java/com/abouerp/library/applet/controller/BookDetailController.java
@@ -4,8 +4,15 @@ import com.abouerp.library.applet.bean.ResultBean;
 import com.abouerp.library.applet.domain.book.BookDetail;
 import com.abouerp.library.applet.domain.book.BookStatus;
 import com.abouerp.library.applet.exception.BookDetailNotFoundException;
+import com.abouerp.library.applet.mapper.BookDetailMapper;
 import com.abouerp.library.applet.service.BookDetailService;
+import com.abouerp.library.applet.vo.BookDetailVO;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Abouerp
@@ -32,6 +39,16 @@ public class BookDetailController {
     @GetMapping("/status")
     public ResultBean getBookStatus() {
         return ResultBean.ok(BookStatus.mappers);
+    }
+
+    /**
+     * 根据图书的id获取该图书的所有馆藏信息
+     * @param bookDetailVO.bookId  图书的id
+     */
+    @GetMapping
+    public ResultBean findAll(BookDetailVO bookDetailVO,
+                              @PageableDefault(sort = "createTime", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResultBean.ok(bookDetailService.findAll(pageable, bookDetailVO).map(BookDetailMapper.INSTANCE::toDTO));
     }
 
 
