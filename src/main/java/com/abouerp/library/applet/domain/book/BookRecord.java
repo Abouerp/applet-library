7ed775558b6dadcf4ff5f8479cd99447441c2a28
Abouerp <1057240821@qq.com>
2021-03-08 01:37:33 +0800

    add bookRecord

---
+++ src/main/java/com/abouerp/library/applet/domain/book/BookRecord.java
@@ -0,0 +1,36 @@
+package com.abouerp.library.applet.domain.book;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+import java.time.Instant;
+
+/**
+ * @author Abouerp
+ */
+@Data
+@Entity
+@Accessors(chain = true)
+public class BookRecord implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Integer id;
+    private String username;
+    private String bookName;
+    private Integer bookDetailId;
+    private BookStatus status;
+    //借书时间
+    private Instant borrowTime;
+    //前端选择借书时间然后返回
+    private Instant returnTime;
+    //web or applet
+    private String borrowWay;
+
+
+
+}
