9743749b841bccaf1ad6c38718f802951455ade4
Abouerp <1057240821@qq.com>
2021-03-10 21:54:54 +0800

    update repository and service

--- src/main/java/com/abouerp/library/applet/repository/BookRecordRepository.java
+++ src/main/java/com/abouerp/library/applet/repository/BookRecordRepository.java
@@ -2,9 +2,12 @@ package com.abouerp.library.applet.repository;
 
 import com.abouerp.library.applet.domain.book.BookRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
 
 /**
  * @author Abouerp
  */
-public interface BookRecordRepository extends JpaRepository<BookRecord,Integer> {
+public interface BookRecordRepository extends JpaRepository<BookRecord,Integer> , QuerydslPredicateExecutor<BookRecord> {
+
 }
--- src/main/java/com/abouerp/library/applet/service/BookRecordService.java
+++ src/main/java/com/abouerp/library/applet/service/BookRecordService.java
@@ -1,7 +1,11 @@
 package com.abouerp.library.applet.service;
 
 import com.abouerp.library.applet.domain.book.BookRecord;
+import com.abouerp.library.applet.domain.book.QBookRecord;
 import com.abouerp.library.applet.repository.BookRecordRepository;
+import com.querydsl.core.BooleanBuilder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,4 +23,12 @@ public class BookRecordService {
     public BookRecord save(BookRecord bookRecord){
         return bookRecordRepository.save(bookRecord);
     }
+
+    public Page<BookRecord> findByUserId(Integer id, Pageable pageable){
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QBookRecord qBookRecord = QBookRecord.bookRecord;
+        booleanBuilder.and(qBookRecord.userId.eq(id));
+        return bookRecordRepository.findAll(booleanBuilder,pageable);
+    }
+
 }
