c0a19ee5ac0c35d3daf2ca7290ac7f6f089f96b3
Abouerp <1057240821@qq.com>
2021-03-16 15:37:03 +0800

    add findByUserId and bookDetailId

--- src/main/java/com/abouerp/library/applet/repository/BookRecordRepository.java
+++ src/main/java/com/abouerp/library/applet/repository/BookRecordRepository.java
@@ -4,10 +4,12 @@ import com.abouerp.library.applet.domain.book.BookRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.Optional;
+
 
 /**
  * @author Abouerp
  */
 public interface BookRecordRepository extends JpaRepository<BookRecord,Integer> , QuerydslPredicateExecutor<BookRecord> {
-
+    Optional<BookRecord> findByUserIdAndBookDetailId(Integer userId, Integer bookDetailId);
 }
